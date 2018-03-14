c2ca94714f7f571849c84b11fd34c49a5faa082c
dowenb00 <34041313+dowenb00@users.noreply.github.com>
2018-03-14 16:28:19 -0500

    Add project files.

---
+++ ConsoleApplication10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aabbccddee";
+            var newstring = "";
+            int count = 0;
+
+            int[] vector = { 1, 3, 5, 7, 9, 20 };
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ( i == vector[count] - 1)
+                {
+                    newstring += char.ToUpper(s[i]);
+                    count++;
+                }
+                else
+                {
+                    newstring += s[i];
+                }    
+            }
+
+            Console.WriteLine(newstring);
+        }
+    }
+}
